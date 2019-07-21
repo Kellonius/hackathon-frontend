88e29820549e11e545deb9f91fa12e33c36a6dc4
bbigham <bryce.bigham@vizientinc.com>
2019-07-20 19:53:45 -0500

    BB

---
+++ src/app/components/medical-reports/medical-reports.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-medical-reports',
+  templateUrl: './medical-reports.component.html',
+  styleUrls: ['./medical-reports.component.sass']
+})
+export class MedicalReportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
